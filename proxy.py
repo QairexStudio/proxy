186d3465ec8a1913248b45e02d86c0f44f970d1c
QairexStudio <58147889+QairexStudio@users.noreply.github.com>
2021-06-27 16:28:47 +0300

    UPDATE

--- proxy.py
+++ proxy.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import time
 import sys
+import os
 
 class proxy:
     def get(*number):
@@ -21,6 +22,7 @@ class proxy:
     def check(ip, port):
         ua = UserAgent()
         combine = str(ip) + ":" + str(port)
+        os.sys.stdout.flush()
         sys.stdout.write("Checking... " + combine)
         try:
             try:
@@ -29,23 +31,37 @@ class proxy:
                         try:
                             req = requests.get("http://httpbin.org/ip", proxies={"http": "http://" + combine, "https": "https://" + combine}, timeout=6)
                             if req.ok:
+                                os.sys.stdout.flush()
                                 sys.stdout.write("\rWorking..." + combine + " " + str(req))
+                                os.sys.stdout.flush()
                                 return True
                             else:
+                                os.sys.stdout.flush()
                                 sys.stdout.write("\rNot working..." + combine + " " + str(req))
+                                os.sys.stdout.flush()
                                 return False
                         except:
+                            os.sys.stdout.flush()
                             sys.stdout.write("\rUnknown proxy error has occurred" + combine)
+                            os.sys.stdout.flush()
                             return False
                     except requests.exceptions.ChunkedEncodingError:
+                            os.sys.stdout.flush()
                             sys.stdout.write("\rProxy connection closed!" + combine)
+                            os.sys.stdout.flush()
                             return False
                 except requests.exceptions.ProxyError:
+                     os.sys.stdout.flush()
                         sys.stdout.write("\rProxy ERROR!" + combine)
+                        os.sys.stdout.flush()
                         return False
             except requests.exceptions.ReadTimeout:
+                os.sys.stdout.flush()
                 sys.stdout.write("\rProxy cannot connect!" + combine)
+                os.sys.stdout.flush()
                 return False
         except requests.exceptions.ConnectTimeout:
+            os.sys.stdout.flush()
             sys.stdout.write("\rProxy Connection Timeout!" + combine)
+            os.sys.stdout.flush()
             return False
